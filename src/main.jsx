c12827de642bbdc2f16254ba2d83e27ae529db5b
Kevin Jair Valenzuela Ponce <19170694@itculiacan.edu.mx>
2024-06-19 13:13:31 -0700

    Buttons Example

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const SpecialButton = ({text}) =>{
+  return(
+    <button>{text}</button>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.Fragment>
+    <SpecialButtonButton text ="Hola"/>
+    <SpecialButtonButton text ="Adios"/>
+    <SpecialButtonButton text ="Hasta Luego"/>        
+
+  </React.Fragment>
 )
