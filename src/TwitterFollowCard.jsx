805ac446a6b8298915fe1d06ac713941dfb152ae
Kevin Jair Valenzuela Ponce <19170694@itculiacan.edu.mx>
2024-06-21 10:20:03 -0700

    Twitter FollowCard Last

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -22,7 +22,8 @@ export function TwitterFollowCard({userName, name, initialIsFollowing, avatarURL
             </header>
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className="tw-followCard-button-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
